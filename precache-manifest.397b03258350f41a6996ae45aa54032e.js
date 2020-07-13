e0fb6c31bb882d7d67c860a519cfd0e35da23077
krystiano13 <krystiano13@interklasa.pl>
2020-07-13 21:51:39 +0200

    Updates

---
+++ precache-manifest.397b03258350f41a6996ae45aa54032e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7058ebe8caef9f2fc8caf87484ec029a",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "98eb77906a04c1e95eb7",
+    "url": "/WeatherApp/static/css/main.3c08a78c.chunk.css"
+  },
+  {
+    "revision": "0ba091a49abac8a0f09e",
+    "url": "/WeatherApp/static/js/2.7d29a79e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.7d29a79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98eb77906a04c1e95eb7",
+    "url": "/WeatherApp/static/js/main.17f6f27b.chunk.js"
+  },
+  {
+    "revision": "1d501c6cbc4900159aea",
+    "url": "/WeatherApp/static/js/runtime-main.46a7f6c7.js"
+  }
+]);
